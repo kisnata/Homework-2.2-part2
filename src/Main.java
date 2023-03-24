f3920a00de95949fb7b64d4e7d5ff9c19f3b0fec
Ната <nata.van4inova@yandex.ru>
2023-03-24 15:47:07 +0300

    В работе

--- src/Main.java
+++ src/Main.java
@@ -3,7 +3,6 @@ public class Main {
         Car car = new Car("car1", 4);
         Car car2 = new Car("car2", 4);
 
-
         Truck truck = new Truck("truck1", 6);
         Truck truck2 = new Truck("truck2", 8);
 
